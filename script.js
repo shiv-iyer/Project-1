8fada43bf3eca07f1db5a9e82405e7d30c21b914
shiv <shiviyer55x@gmail.com>
2023-02-15 20:57:59 +0800

    linked style.css (...) and added functional base map

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 
+// Initialize the variables
+
+const map = L.map("singaporeMap");
 const fourSquareURL = "https://api.foursquare.com/v3/places/search";
 const FOURSQUARE_API_KEY = "fsq3jMP2VLbUVepHLNvSdnHHmMc7KnQT7vAYp7wRcl+DPvU=";
 const sgLatLong = "1.290270,103.851959";
+const searchButton = document.getElementById("btnSearch");
+
+// Unsure whether to write functions at the top or bottom (bc they are hoisted anyways). Will do bottom for now... used to from Java
+
+// Load Leaflet Map 
+map.setView([1.290270, 103.851959], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+searchButton.addEventListener('click', function(){
+    alert("Hello World");
+    const searchValue = document.getElementById("searchValue").value;
+    loadData(fourSquareURL, sgLatLong, "pet cafe");
+});
+
+// ALL FUNCTIONS
+
+function transformLatLng(initialLL){
+    return(initialLL.split(","));
+}
 
 // async function to load the data from axios
 // Quotations are optional for the key names. They are just so we know they are strings
@@ -21,11 +48,3 @@ async function loadData(url, latLong, searchValue){
 
     console.log(response.data);
 }
-
-const searchButton = document.getElementById("btnSearch");
-
-searchButton.addEventListener('click', function(){
-    alert("Hello World");
-    const searchValue = document.getElementById("searchValue").value;
-    loadData(fourSquareURL, sgLatLong, "pet cafe");
-});
