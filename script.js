123ed5c68ac710002788c0eaeb833313c0521151
shiv <shiviyer55x@gmail.com>
2023-02-16 14:54:24 +0800

    added custom marker icon

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const searchButton = document.getElementById("btnSearch");
 // add marker1 with any coordinates, will use Bishan Dog Park
 const marker1 = L.marker([1.362372, 103.84859]);
 
+// marker icon will not have a shadow for now
+const markerIcon = L.icon({
+    iconUrl: '/images/pawbase-border.png',
+
+    iconSize:     [50, 50], // size in pixels
+    //iconAnchor:   [22, 94], 
+    //popupAnchor:  [-3, -76] 
+});
+
+// custom marker icon test at Duxton Plain Dog Run
+const markerCustomIcon = L.marker([1.277601, 103.841688], {icon: markerIcon});
+
 // add circle with any coordinates, will use Commonwealth Dog Run
 const circle1 = L.circle([1.304987, 103.797131], {
     color: 'red',
@@ -36,6 +48,9 @@ marker1.addTo(map);
 // next step: add popups to the map layers
 marker1.bindPopup("I am a marker at Bishan Dog Park!");
 circle1.bindPopup("I am a circle at Commonwealth Dog Run!");
+markerCustomIcon.bindPopup("I am a marker with a custom icon at Duxton Plain!");
+
+markerCustomIcon.addTo(map);
 
 loadData(fourSquareURL);
 
@@ -45,7 +60,7 @@ const testPopup = L.popup()
     .setContent("Standalone popup test at PIE Dog Run!")
     .openOn(map);
 
-// add
+// add clickable functionality for anywhere on the map
 let clickyPopup = L.popup();
 
 function onMapClick(e) {
