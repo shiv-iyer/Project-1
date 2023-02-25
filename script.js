9f6dd7e1f5f751b7b1471bb84053daeea963479a
shiv <shiviyer55x@gmail.com>
2023-02-25 16:05:56 +0800

    custom marker icon functionality for dog parks

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const FOURSQUARE_API_KEY = "fsq3jMP2VLbUVepHLNvSdnHHmMc7KnQT7vAYp7wRcl+DPvU=";
 const sgLatLong = "1.290270,103.851959";
 const searchButton = document.getElementById("btnSearch");
 
+// Custom Leaflet marker icon for dog parks, it's a paw
+const dogParkIcon = L.icon({
+    // worry about other variables / options later, first, establish basic functionality for one category
+    iconUrl: '/images/pawbase-border.png',
+    iconSize: [35, 35]
+});
+
 // Load Leaflet Map 
 map.setView([1.290270, 103.851959], 13);
 
@@ -59,7 +66,6 @@ searchButton.addEventListener('click', function(){
         // maybe don't need default? since it is only 1 of 4 options rn
     }
 
-    console.log(searchCategory);
     loadData(fourSquareURL, searchCategory, searchLayer);
 });
 
@@ -92,18 +98,27 @@ async function loadData(url, searchType, layerType){
     // get the total amount of search results, and plot as many amount of markers.
     // fourSquare's results is an array of objects. Use Object.keys to get as many keys to know the length
     const searchResultsLength = Object.keys(response.data.results).length;
-    console.log(searchResultsLength);
 
     // traverse through from 0 to the amount of search results
     for (let i = 0; i < searchResultsLength; i++){
         // obtain geocodes: retrieve the coordinates of each result
         const queryGeocodes = queryResults.results[i].geocodes.main;
-        console.log(queryGeocodes);
 
         // queryGeocodes returns an object with keys that store latitude and longitude.
         // seeing as Leaflet markers require an array of coords, store as that first, hard code it
         // there has to be an elegant way to make it from an object into an array look into later!!!
-        const resultMarker = L.marker([queryGeocodes.latitude, queryGeocodes.longitude]);
+
+        console.log(layerType == dogParksLayer);
+
+        // if layer type = dog parks layer, add custom icon, else normal
+
+        let resultMarker;
+        if (layerType == dogParksLayer){
+            resultMarker = L.marker([queryGeocodes.latitude, queryGeocodes.longitude], {icon: dogParkIcon});
+        } else {
+            resultMarker = L.marker([queryGeocodes.latitude, queryGeocodes.longitude]);
+        }
+
         resultMarker.bindPopup("This is a marker displaying " + queryResults.results[i].name);
 
         // can maybe store results as a nicer-formatted object later, JUST make it functional first
