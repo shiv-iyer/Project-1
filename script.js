eea9f02bcb30bb4fec60645b8359a1182193e7a9
shiv <shiviyer55x@gmail.com>
2023-03-04 14:37:49 +0800

    added basic search by place

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const FOURSQUARE_API_KEY = "fsq3jMP2VLbUVepHLNvSdnHHmMc7KnQT7vAYp7wRcl+DPvU=";
 const sgLatLong = "1.290270,103.851959";
 const searchButton = document.getElementById("btnSearch");
 
-let resultForm;
+// initialize some variables that will be assigned after dom content loaded
+let queryForm;
 let radioButtons;
+let resultForm;
 
 // Custom Leaflet marker icon for dog parks, it's a paw
 const dogParkIcon = L.icon({
@@ -51,6 +53,10 @@ const layerController = L.control.layers(
 
 // Event Listener for search button: on click
 searchButton.addEventListener('click', function(){
+
+    const userQuery = queryForm.value;
+    console.log("User query: " + userQuery);
+
     const selectedCategory = document.querySelector("#categoryForm").value;
     let searchCategory;
     //alert("Category Form value: " + selectedCategory);
@@ -83,10 +89,10 @@ searchButton.addEventListener('click', function(){
 
     // first if everything is blank
     if (!resultForm.value && checkRadios() == 3){
-        alert("Nothing is selected!");
+        alert("Please select the number of results you would like to display!");
     } else if (checkRadios() == 3) {
         if (isNaN(Number(resultForm.value))){
-            alert("You did not input a valid number, please try again!");
+            alert("Your custom results input was not a valid number, please try again!");
             // now, ensure that the user inputted results from 10 to 50 only
         } else if (resultForm.value < 10 || resultForm.value > 50){
             alert("You need to input a number from 10-50!");
@@ -96,6 +102,7 @@ searchButton.addEventListener('click', function(){
     } else {
         // switch to get selected radio button
         resultLimit = 25;
+        loadData(fourSquareURL, userQuery, searchCategory, searchLayer, resultLimit);
     }
 
     // now, retrieve the data from the results form
@@ -105,9 +112,6 @@ searchButton.addEventListener('click', function(){
 
     // if results form is left blank, perform validation and avoid calling any functions
     // can refactor this into a switch later
-    
-
-    loadData(fourSquareURL, searchCategory, searchLayer, resultLimit);
     }
 
 );
@@ -116,7 +120,7 @@ searchButton.addEventListener('click', function(){
 // async function to load the data from axios
 // Quotations are optional for the key names. They are just so we know they are strings
 // add back: latLong and searchValue in the params
-async function loadData(url, searchType, layerType, resultLimit){
+async function loadData(url, userQuery, searchType, layerType, resultLimit){
     console.log("Search category: " + searchType);
     const response = await axios.get(url, {
         headers: {
@@ -127,6 +131,7 @@ async function loadData(url, searchType, layerType, resultLimit){
         },
         // parameters for FourSquare search
         "params":{
+            query: userQuery,
             ll: sgLatLong,
             categories: searchType,
             limit: resultLimit
@@ -142,77 +147,82 @@ async function loadData(url, searchType, layerType, resultLimit){
     // fourSquare's results is an array of objects. Use Object.keys to get as many keys to know the length
     const searchResultsLength = Object.keys(response.data.results).length;
 
-    // traverse through from 0 to the amount of search results
-    for (let i = 0; i < searchResultsLength; i++){
-        // obtain geocodes: retrieve the coordinates of each result
-        const queryGeocodes = queryResults.results[i].geocodes.main;
-
-        // queryGeocodes returns an object with keys that store latitude and longitude.
-        // seeing as Leaflet markers require an array of coords, store as that first, hard code it
-        // there has to be an elegant way to make it from an object into an array look into later!!!
-
-        console.log(layerType == dogParksLayer);
-
-       // if layer type = dog parks layer, add custom icon, else normal
-
-       let resultMarker;
-
-       // later, can do a switch on layerType
-       if (layerType == dogParksLayer){
-           resultMarker = L.marker([queryGeocodes.latitude, queryGeocodes.longitude], {icon: dogParkIcon});
-       } else {
-           resultMarker = L.marker([queryGeocodes.latitude, queryGeocodes.longitude]);
-       }
-       
-       // retrieve results from API query
-       const name = queryResults.results[i].name;
-       const formattedAddress = queryResults.results[i].location.formatted_address;
-
-       // create the card, first container
-       const cardContainer = document.createElement("div");
-       cardContainer.setAttribute("class", "card");
-       // next, card body
-       const cardBody = document.createElement("div");
-       cardBody.setAttribute("class", "card-body");
-       // now, card title
-       const cardTitle = document.createElement("h5");
-       cardTitle.setAttribute("class", "card-title");
-       cardTitle.innerText = name;
-
-       // now, child card text
-       const cardText = document.createElement("p");
-       cardText.setAttribute("class", "card-text");
-       cardText.innerText = formattedAddress;
-
-       // possible todo: secondary "Type: Pet Cafe" for whatever category it is
-       // image testing
-       let imageUrl = queryResults.results[i].categories[0].icon.prefix;
-       const imageSuffix = ".png";
-       imageUrl += imageSuffix;
-       console.log("image url: " + imageUrl);
-       
-       // append stuff
-       cardBody.appendChild(cardTitle);
-       cardBody.appendChild(cardText);
-       cardContainer.appendChild(cardBody);
-
-       resultMarker.bindPopup(cardContainer);
-        //resultMarker.bindPopup("This is a marker displaying " + queryResults.results[i].name);
-
-        // add event listener to fly to marker on click
-        resultMarker.addEventListener('click', function(){
-            map.flyTo([queryGeocodes.latitude, queryGeocodes.longitude], 17);
-        });
-
-
-        // add marker to the layer group
-        resultMarker.addTo(layerType);
+    if (searchResultsLength == 0){
+        alert("No search results found!");
+    } else {
+        // traverse through from 0 to the amount of search results
+        for (let i = 0; i < searchResultsLength; i++){
+            // obtain geocodes: retrieve the coordinates of each result
+            const queryGeocodes = queryResults.results[i].geocodes.main;
+    
+            // queryGeocodes returns an object with keys that store latitude and longitude.
+            // seeing as Leaflet markers require an array of coords, store as that first, hard code it
+            // there has to be an elegant way to make it from an object into an array look into later!!!
+    
+            console.log(layerType == dogParksLayer);
+    
+           // if layer type = dog parks layer, add custom icon, else normal
+    
+           let resultMarker;
+    
+           // later, can do a switch on layerType
+           if (layerType == dogParksLayer){
+               resultMarker = L.marker([queryGeocodes.latitude, queryGeocodes.longitude], {icon: dogParkIcon});
+           } else {
+               resultMarker = L.marker([queryGeocodes.latitude, queryGeocodes.longitude]);
+           }
+           
+           // retrieve results from API query
+           const name = queryResults.results[i].name;
+           const formattedAddress = queryResults.results[i].location.formatted_address;
+    
+           // create the card, first container
+           const cardContainer = document.createElement("div");
+           cardContainer.setAttribute("class", "card");
+           // next, card body
+           const cardBody = document.createElement("div");
+           cardBody.setAttribute("class", "card-body");
+           // now, card title
+           const cardTitle = document.createElement("h5");
+           cardTitle.setAttribute("class", "card-title");
+           cardTitle.innerText = name;
+    
+           // now, child card text
+           const cardText = document.createElement("p");
+           cardText.setAttribute("class", "card-text");
+           cardText.innerText = formattedAddress;
+    
+           // possible todo: secondary "Type: Pet Cafe" for whatever category it is
+           // image testing
+           let imageUrl = queryResults.results[i].categories[0].icon.prefix;
+           const imageSuffix = ".png";
+           imageUrl += imageSuffix;
+           console.log("image url: " + imageUrl);
+           
+           // append stuff
+           cardBody.appendChild(cardTitle);
+           cardBody.appendChild(cardText);
+           cardContainer.appendChild(cardBody);
+    
+           resultMarker.bindPopup(cardContainer);
+            //resultMarker.bindPopup("This is a marker displaying " + queryResults.results[i].name);
+    
+            // add event listener to fly to marker on click
+            resultMarker.addEventListener('click', function(){
+                map.flyTo([queryGeocodes.latitude, queryGeocodes.longitude], 17);
+            });
+    
+            // add marker to the layer group
+            resultMarker.addTo(layerType);
+        }
     }
 }
 
 window.addEventListener("DOMContentLoaded", function(){
+    queryForm = document.getElementById("queryForm");
     radioButtons = document.querySelectorAll(".radios");
     resultForm = document.getElementById("resultLimitForm");
+    console.log(queryForm);
     console.log(resultForm);
 
     resultForm.addEventListener('input', function(){
